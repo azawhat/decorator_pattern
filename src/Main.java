d7ad75b06cc0e489f52d55087c1ae22e785232ef
azawhat <azamat.erezhepbayev@gmail.com>
2023-10-07 23:59:28 +0600

    assignment

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car baseCar = new BasicCar();
+        Car gpsCar = new GPS(baseCar);
+        Car leatherSeatsCar = new Seats(gpsCar);
+
+        System.out.println("These things are yours " + leatherSeatsCar.getDescription());
+        System.out.println("It will cost you $" + leatherSeatsCar.getCost());
     }
 }
